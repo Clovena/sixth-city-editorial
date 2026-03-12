815e37cfadd306af6c48efd55337eb30a136c88f
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2026-03-12 10:13:11 -0400

    Content page housekeeping.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import { remarkTeamHeaders } from './src/lib/remark-team-headers.ts';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  markdown: {
+    remarkPlugins: [remarkTeamHeaders],
   }
 });
